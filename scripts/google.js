39581a4b065de65987bcee21855da2da4095f730
MogasbeKelani <MogasbeKelani@gmail.com>
2021-12-23 21:58:26 -0800

    adding logging

--- scripts/google.js
+++ scripts/google.js
@@ -37,8 +37,8 @@ function initClient() {
     updateSigninStatus(gapi.auth2.getAuthInstance().isSignedIn.get());
     authorizeButton.onclick = handleAuthClick;
     signoutButton.onclick = handleSignoutClick;
-  }, function(error) {
-      console.log(error);
+  }, function (error) {
+    console.log(error);
     appendPre(JSON.stringify(error, null, 2));
   });
 }
@@ -86,15 +86,16 @@ function appendPre(message) {
 
 /**
  * Print the names and majors of students in a sample spreadsheet:
- * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
+ * https://docs.google.com/spreadsheets/d/1nkvk4K0k7ZU2MEh_8Yt7Tb1Q6zUqKbaorR7cOaM_RUY/edit
  */
 function listMajors() {
   gapi.client.sheets.spreadsheets.values.get({
     spreadsheetId: '1nkvk4K0k7ZU2MEh_8Yt7Tb1Q6zUqKbaorR7cOaM_RUY',
     range: 'Class Data!A2:K2',
-  }).then(function(response) {
+  }).then(function (response) {
     console.log(response);
     var range = response.result;
+    console.log('range', range);
     if (range.values.length > 0) {
       appendPre('Name, Major:');
       for (i = 0; i < range.values.length; i++) {
@@ -107,115 +108,115 @@ function listMajors() {
     }
     range.values.push(['hello', 'mo'])
     gapi.client.sheets.spreadsheets.values.update({
-    spreadsheetId: '1nkvk4K0k7ZU2MEh_8Yt7Tb1Q6zUqKbaorR7cOaM_RUY',
-    range: 'Class Data!A2:E',
-    valueInputOption: 'USER_ENTERED',
-    resource: {
-      values: range.values,
-    }
-  }).then(function(response){
+      spreadsheetId: '1nkvk4K0k7ZU2MEh_8Yt7Tb1Q6zUqKbaorR7cOaM_RUY',
+      range: 'Class Data!A2:E',
+      valueInputOption: 'USER_ENTERED',
+      resource: {
+        values: range.values,
+      }
+    }).then(function (response) {
 
-    console.log(response);
+      console.log(response);
 
-  });
-  }, function(response) {
+    });
+  }, function (response) {
     console.log(response);
     appendPre('Error: ' + response.result.error.message);
   });
 
 }
-document.getElementById("submit").onclick =  () => {
+document.getElementById("submit").onclick = () => {
 
-    const check = checkAllValues();
-    if(check.error){
-        alert(check.message)
-    }
+  const check = checkAllValues();
+  if (check.error) {
+    alert(check.message)
+  }
 
-   console.log(check);
+  console.log(check);
 }
-function checkAllValues () {
-    const classValue = document.querySelector('input[name="Class"]:checked');
-    const fname = document.getElementById('fname').value;
-    const lname = document.getElementById('lname').value;
-    const gender = document.getElementById('gender').value;
-    const DOB = document.getElementById('DOB').value;
-    const schoolYear = document.getElementById('school-year').value;
-    const emiratesId = document.getElementById('emiratesId').value;
-    const vaccine1 = document.querySelector('input[name="vaccine#1"]:checked')
-    const vaccine2 = document.querySelector('input[name="vaccine#2"]:checked')
-    const otherExplanation = document.getElementById('otherExplanation').value;
-    const datevaccine1 = document.getElementById('datevaccine1').value;
-    const datevaccine2 = document.getElementById('datevaccine2').value;
-    const datevaccineb = document.getElementById('datevaccineb').value;
+function checkAllValues() {
+  const classValue = document.querySelector('input[name="Class"]:checked');
+  const fname = document.getElementById('fname').value;
+  const lname = document.getElementById('lname').value;
+  const gender = document.getElementById('gender').value;
+  const DOB = document.getElementById('DOB').value;
+  const schoolYear = document.getElementById('school-year').value;
+  const emiratesId = document.getElementById('emiratesId').value;
+  const vaccine1 = document.querySelector('input[name="vaccine#1"]:checked')
+  const vaccine2 = document.querySelector('input[name="vaccine#2"]:checked')
+  const otherExplanation = document.getElementById('otherExplanation').value;
+  const datevaccine1 = document.getElementById('datevaccine1').value;
+  const datevaccine2 = document.getElementById('datevaccine2').value;
+  const datevaccineb = document.getElementById('datevaccineb').value;
 
-    
-    let errorMessage = 'You Forgot The Following Fields: '
-    let isError = false;
-    if(!classValue) {
-        errorMessage += '(class) '
-        isError = true;
-    }
-    if(!fname) {
-        errorMessage += '(first name) '
-        isError = true;
-    }
-    if(!lname) {
-        errorMessage += '(last name) '
-        isError = true;
-    }
-    if(!gender) {
-        errorMessage += '(gender) '
-        isError = true;
-    }
-    if(!DOB) {
-        errorMessage += '(date of birth)'
-        isError = true;
-    }
-    if(!schoolYear) {
-        errorMessage += '(school-year)'
-        isError = true;
-    }
-    if(!emiratesId) {
-        errorMessage += '(emiratesId)'
-        isError = true;
-    }
-    if(!vaccine1) {
-        errorMessage += '(vaccine1)'
-        isError = true;
-    }
-    else if(!vaccine2) {
-        errorMessage += '(vaccine2)'
-        isError = true;
-    }
-    else if((vaccine1.value == 'other' || vaccine2.value == 'other') && !otherExplanation) {
-        errorMessage += '(other)'
-        isError = true;
-    }
-    if(!datevaccine1) {
-        errorMessage += '(datevaccine1)'
-        isError = true;
-    }
-    if(!datevaccine2) {
-        errorMessage += '(datevaccine2)'
-        isError = true;
-    }
-    if(!datevaccineb) {
-        errorMessage += '(datevaccineb)'
-        isError = true;
-    }
-    return { 
-        error: isError, 
-        message: errorMessage, 
-        fname, 
-        lname, 
-        gender,
-        DOB,
-        schoolYear,
-        emiratesId,
-        vaccine1,
-        vaccine2, 
-        datevaccine1,
-        datevaccine2,
-        datevaccineb,
-     };
+
+  let errorMessage = 'You Forgot The Following Fields: '
+  let isError = false;
+  if (!classValue) {
+    errorMessage += '(class) '
+    isError = true;
+  }
+  if (!fname) {
+    errorMessage += '(first name) '
+    isError = true;
+  }
+  if (!lname) {
+    errorMessage += '(last name) '
+    isError = true;
+  }
+  if (!gender) {
+    errorMessage += '(gender) '
+    isError = true;
+  }
+  if (!DOB) {
+    errorMessage += '(date of birth)'
+    isError = true;
+  }
+  if (!schoolYear) {
+    errorMessage += '(school-year)'
+    isError = true;
+  }
+  if (!emiratesId) {
+    errorMessage += '(emiratesId)'
+    isError = true;
+  }
+  if (!vaccine1) {
+    errorMessage += '(vaccine1)'
+    isError = true;
+  }
+  else if (!vaccine2) {
+    errorMessage += '(vaccine2)'
+    isError = true;
+  }
+  else if ((vaccine1.value == 'other' || vaccine2.value == 'other') && !otherExplanation) {
+    errorMessage += '(other)'
+    isError = true;
+  }
+  if (!datevaccine1) {
+    errorMessage += '(datevaccine1)'
+    isError = true;
+  }
+  if (!datevaccine2) {
+    errorMessage += '(datevaccine2)'
+    isError = true;
+  }
+  if (!datevaccineb) {
+    errorMessage += '(datevaccineb)'
+    isError = true;
+  }
+  return {
+    error: isError,
+    message: errorMessage,
+    fname,
+    lname,
+    gender,
+    DOB,
+    schoolYear,
+    emiratesId,
+    vaccine1,
+    vaccine2,
+    datevaccine1,
+    datevaccine2,
+    datevaccineb,
+  };
 }
